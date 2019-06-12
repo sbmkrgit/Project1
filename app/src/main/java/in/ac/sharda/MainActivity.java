c63bca578fd4b50dcb19c70f88f403be5202ceee
SHUBHAM <babukumar9931@gmail.com>
2019-06-12 21:58:24 +0530

    Initial commit

---
+++ app/src/main/java/in/ac/sharda/MainActivity.java
@@ -0,0 +1,55 @@
+package in.ac.sharda;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public static final String EXTRA_TEXT = "in.ac.sharda.EXTRA_TEXT";
+    EditText userName, password;
+    Button btnSubmit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        userName = (EditText) findViewById(R.id.userName);
+        password = (EditText) findViewById(R.id.password);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+
+
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username1 = userName.getText().toString();
+                String password1 = password.getText().toString();
+                if(TextUtils.isEmpty(username1)){
+                    userName.setError("Enter User Name");
+                }
+                else if(TextUtils.isEmpty(password1)){
+                    password.setError("Enter password");
+                }
+                else{
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    intent.putExtra(EXTRA_TEXT, username1 );
+                    startActivity(intent);
+
+                }
+            }
+        });
+
+
+    }
+
+
+}
---
+++ app/src/main/java/in/ac/sharda/SecondActivity.java
@@ -0,0 +1,22 @@
+package in.ac.sharda;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView txt1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String text = intent.getStringExtra(MainActivity.EXTRA_TEXT );
+        TextView txt1 = (TextView)findViewById(R.id.txt1);
+        txt1.setText("Welcome "+ text + " !!" );
+
+    }
+}
